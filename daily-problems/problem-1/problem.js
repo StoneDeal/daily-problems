496b2fde5f939cc452570129b0cd9fdd770a67be
StoneDeal <stonedeal23@gmail.com>
2020-09-07 15:07:17 -0500

    Update problem.js

--- daily-problems/problem-1/problem.js
+++ daily-problems/problem-1/problem.js
@@ -1,4 +1,7 @@
-
+/*
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17
+*/
 //Set variables.
 var list = [10, 4, 5, 7];
 var k = 17;
@@ -8,7 +11,7 @@ var trueCounter = 0;
 for(i=0; i<list.length; i++){
     for(j=0; j<list.length; j++){ //Loop through for second number.
         if (i==j){ 
-            break; //Make sure you are not adding the list index to itself.
+            break; //Make sure you are not adding the same list index to itself.
         }
         else{
             var a = list[i] + list[j] //Add numbers together.
